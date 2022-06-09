782e411593dd2fb98b6274a63b0ea76ad4cdd5d5
Cory Zue <cory@coryzue.com>
2022-06-09 12:00:42 +0200

    switch to furo theme

--- conf.py
+++ conf.py
@@ -78,7 +78,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
