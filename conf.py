8bbd23cb5cf8a6e0166ac7669c57f23f36b069d0
David Garcia <hi@davidgarcia.dev>
2023-08-04 10:19:31 +0100

    Install pushfeedback with Sphinx extension

--- conf.py
+++ conf.py
@@ -44,10 +44,11 @@ extensions = [
     'myst_parser',
     'sphinxcontrib.gtagjs',
     'sphinx_reredirects',
+    'sphinx_pushfeedback',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -107,14 +108,6 @@ html_static_path = ['_static']
 # 'searchbox.html']``.
 #
 # html_sidebars = {}
-
-# pushfeedback config
-html_css_files = [
-    'https://cdn.jsdelivr.net/npm/pushfeedback/dist/pushfeedback/pushfeedback.css',
-]
-html_js_files = [
-    'https://cdn.jsdelivr.net/npm/pushfeedback/dist/pushfeedback/pushfeedback.esm.js',
-]
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
@@ -205,3 +198,7 @@ redirects = {
     "api-keys": "apis.html",
     "experimental": "css/tailwind.html",
 }
+
+# pushfeedback configuration
+
+pushfeedback_project="hiz6sjmrnx"
